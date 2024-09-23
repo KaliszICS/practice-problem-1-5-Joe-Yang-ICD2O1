24b0c6d3464aba76de755f75b72ce65bace50d34
Joe-Yang-ICD2O1 <joe.yang@student.tdsb.on.ca>
2024-09-23 13:51:09 +0000

    answer

--- PP1_5.py
+++ PP1_5.py
@@ -2,17 +2,42 @@
 def q1():
   #Write Assignment code here
 
+  integer= input("Input an integer")
+  integer= int(integer)
+  print(integer+ 3)
+
 def q2():
   #Write Assignment code here
 
+  integ= input("Input a number")
+  integ = integ + "4"
+  integ= float(integ)
+  print(integ + 2)
+
+  
 def q3():
   #Write Assignment code here
+  
+  radius= input("Input a radius")
+  radius= float(radius)
+  print(radius * radius * 3.14 )
+
 
 def q4():
   #Write Assignment code here
 
+  number= input("Input a number")
+  number= float(number)
+  print (int(number * 12))
+
+
 def q5():
   #Write Assignment code here
+
+  numb= input("Input an integer")
+  numb= int(numb)
+  numb= (numb + 5)
+  print (f"Your number + 5 is {numb}")
 
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
